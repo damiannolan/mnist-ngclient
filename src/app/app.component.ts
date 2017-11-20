9de22e1d0a654b127d5574c83a187c11380368cb
Damian Nolan <damiannolan@gmail.com>
2017-11-20 15:26:17 +0000

    moved upload logic from app to upload component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,51 +6,7 @@ import 'rxjs/add/operator/catch';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private visible: boolean = false;
-  private apiEndPoint: string = 'http://localhost:5000/upload';
-  private image;
-  private digit: number = 0;
 
-  constructor(private http: Http) {}
+  constructor() {}
 
-  predict() {
-    this.visible = true;
-  }
-
-  imageUpload(evt) {
-    // https://stackoverflow.com/questions/39074806/how-to-preview-picture-stored-in-the-fake-path-in-angular-2-typescript
-    // https://stackoverflow.com/questions/39933340/angular2-display-image
-    let fileList: FileList = evt.target.files;
-    //let file = fileList[0];
-    this.image = fileList[0];
-
-    let reader = new FileReader();
-    reader.onload = (e: any) => {
-      this.image = e.target.result;
-    }
-
-    reader.readAsDataURL(this.image);
-    console.log(this.image);
-    /*
-      - Take this logic and put it into the predict() function
-    */
-    if(fileList[0] && this.image) {
-        console.log('posting');
-        let file: File = fileList[0];
-        let formData:FormData = new FormData();
-        formData.append('file', file, file.name);
-        let headers = new Headers();
-
-        //let options = new RequestOptions({ headers: headers, responseType:ResponseContentType.Blob });
-        let options = new RequestOptions({ headers: headers });
-        this.http.post(`${this.apiEndPoint}`, formData, options)
-            .map(res => res.json())
-            .subscribe((body) => {
-              // return the body where the digit prediction will be held
-              console.log(body);
-              //this.image = window.URL.createObjectURL(blob);
-              this.digit = body;
-          });
-    }
-  }
 }
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-upload',
@@ -7,10 +12,54 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class UploadComponent implements OnInit {
+  private visible: boolean = false;
+  private apiEndPoint: string = 'http://localhost:5000/upload';
+  private image;
+  private digit: number = 0;
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
 
+  predict() {
+    this.visible = true;
+  }
+
+  imageUpload(evt) {
+    // https://stackoverflow.com/questions/39074806/how-to-preview-picture-stored-in-the-fake-path-in-angular-2-typescript
+    // https://stackoverflow.com/questions/39933340/angular2-display-image
+    let fileList: FileList = evt.target.files;
+    //let file = fileList[0];
+    this.image = fileList[0];
+
+    let reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.image = e.target.result;
+    }
+
+    reader.readAsDataURL(this.image);
+    console.log(this.image);
+    /*
+      - Take this logic and put it into the predict() function
+    */
+    if(fileList[0] && this.image) {
+        console.log('posting');
+        let file: File = fileList[0];
+        let formData:FormData = new FormData();
+        formData.append('file', file, file.name);
+        let headers = new Headers();
+
+        //let options = new RequestOptions({ headers: headers, responseType:ResponseContentType.Blob });
+        let options = new RequestOptions({ headers: headers });
+        this.http.post(`${this.apiEndPoint}`, formData, options)
+            .map(res => res.json())
+            .subscribe((body) => {
+              // return the body where the digit prediction will be held
+              console.log(body);
+              //this.image = window.URL.createObjectURL(blob);
+              this.digit = body;
+          });
+    }
+  }
 }
